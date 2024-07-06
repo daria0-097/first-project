b6cf6f223bf14532ddb0f355eca2f6f0a7045ed5
bosMC <bostanAF@gmail.com>
2024-07-06 22:52:46 +0700

    начали декомпозицию, но немного не доделали

--- additional_functions.py
+++ additional_functions.py
@@ -1,4 +1,7 @@
+import requests
+
 from datetime import datetime
+import json
 
 
 def check_correctly_date(user_date: str) -> bool:
@@ -19,5 +22,68 @@ def check_correctly_date(user_date: str) -> bool:
             return False
         return True
 
+
+# вернуться к нормальным названиям функций
+def open_and_read_db(data_key: str):
+    """Задача функции - отрыть файл с датами, попробовать найти ключ и вернуть True or False
+    Возможные варианты развития событий:
+    1) Даты (ключа в словаре) нет! -> 1000% должны сделать запрос к API
+    2) Дата есть! -> запрос мы уже делали, НО!
+        а) если у ключа значение False -> на этом наш скрипт должен закончить работу
+        б) если у ключа значение True -> должны перейти к открытию файл...
+    data_key - это строка вида %d_%m_%Y
+    """
+    with open('info_about_successful_requests.json', 'r', encoding='utf-8') as file:
+        info_about_all_dates: dict = json.load(file)
+
+    if data_key not in info_about_all_dates:
+        return False
+
+    else:
+        flag = info_about_all_dates[data_key]
+        if flag:
+            # должны как-то сообщить основному циклу о том, что нужно открыть файл
+            pass
+        else:
+            return False
+
+
+# вернуться к нормальным названиям функций
+def open_and_read_data_from_file(data_key: str) -> dict:
+    """
+
+    """
+    with open(f'data_base/{data_key}.json', 'r', encoding='utf-8') as file:
+        json_data_from_api = json.load(file)
+
+    return json_data_from_api['Valute']
+
+
+def request_to_currency_api(object_dt: datetime) -> bool | dict:
+    with open('info_about_successful_requests.json', 'r', encoding='utf-8') as file:
+        info_about_all_dates = json.load(file)
+
+    date_key: str = object_dt.strftime('%d_%m_%Y')
+
+    url = f'https://www.cbr-xml-daily.ru/archive/{object_dt.strftime("%Y/%m/%d")}/daily_json.js'
+    response = requests.get(url)
+    json_data_from_api = response.json()
+
+    if response.status_code != 200:
+        info_about_all_dates[date_key] = False
+    else:
+        info_about_all_dates[date_key] = True
+
+        # сохранить полученную информацию
+        with open(f'data_base/{date_key}.json', 'w', encoding='utf-8') as file:
+            json.dump(json_data_from_api, file, indent=4, ensure_ascii=False)
+
+    with open('info_about_successful_requests.json', 'w', encoding='utf-8') as file:
+        json.dump(info_about_all_dates, file, indent=4, ensure_ascii=False)
+
+    if info_about_all_dates[date_key]:
+        return json_data_from_api['Valute']
+
+    return False
 
 
--- main.py
+++ main.py
@@ -5,25 +5,8 @@ from requests.models import Response
 from datetime import datetime
 import json
 
-from additional_functions import check_correctly_date
+from additional_functions import check_correctly_date, open_and_read_data_from_file, open_and_read_db
 
-
-# url = f'https://www.cbr-xml-daily.ru/archive/2024/05/15/daily_json.js'
-# response = requests.get(url)
-#
-# # GET
-# # POST
-#
-# print(response.text)
-
-
-# user_date: str = input()
-# object_dt = datetime.strptime(user_date, '%d.%m.%Y')
-# url = f'https://www.cbr-xml-daily.ru/archive/{object_dt.strftime("%Y/%m/%d")}/daily_json.js'
-
-# print(url)
-# r = requests.get(url)
-# print(r.text)
 
 date_from_input = input('Введи дату в формате dd.mm.yyyy: ')
 flag = False
@@ -32,8 +15,16 @@ if check_correctly_date(date_from_input):
     object_dt = datetime.strptime(date_from_input, '%d.%m.%Y')
     date_key: str = object_dt.strftime('%d_%m_%Y')
 
+    result = open_and_read_db(date_key)
+    if isinstance(result, bool):
+        print('error')
+
+    #
+
     with open('info_about_successful_requests.json', 'r', encoding='utf-8') as file:
         info_about_all_dates: dict = json.load(file)
+
+    result = True
 
     if date_key not in info_about_all_dates:
         print('отправляем запрос')
@@ -57,13 +48,11 @@ if check_correctly_date(date_from_input):
     else:
         if info_about_all_dates[date_key] is True:
             print('такой запрос мы уже отправляли и получили успешный результат!')
-            with open(f'data_base/{date_key}.json', 'r', encoding='utf-8') as file:
-                json_data_from_api = json.load(file)
-
-            print(json_data_from_api)
+            print(open_and_read_data_from_file(data_key=date_key))
         else:
             print('такой запрос мы уже отправляли и получили ошибку (скорей всего были выходные)!')
 
+    # 
 
 else:
     print('error')
