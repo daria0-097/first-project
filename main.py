1786c1e73554667eea4d6177686aa778982c3ed5
bosMC <bostanAF@gmail.com>
2024-08-07 22:34:09 +0700

    start ветки без js кода в index.html

--- main.py
+++ main.py
@@ -49,18 +49,7 @@ def index():
 
 @app.route('/submit', methods=["POST"])
 def submit():
-    data = request.get_json()
-    data_string = f'{data["day"]}.{data["month"]}.{data["year"]}'
-    print(f'Предполагаемая дата:', data_string)
-
-    if check_correctly_date(data_string):
-        result = get_final_data(
-            f'{data["day"]}_{data["month"]}_{data["year"]}',
-            datetime(day=int(data['day']), month=int(data['month']), year=int(data['year']))
-        )
-        print(result)
-
-    return render_template('index.html')
+    pass
 
 
 
