55d0ee8b0cd416b951460658401cde9366266c20
Daria <daria_kovaleva_2109@mail.ru>
2024-09-14 12:39:52 +0300

    finish project

---
+++ functions.py
@@ -0,0 +1,137 @@
+import requests
+from datetime import datetime, timedelta
+import json
+
+def check_correctly_date(user_date: str) -> bool:
+    """Задачи функции:
+    а) проверить, похоже ли то что ввел пользователь на дату?
+    б) если похоже, то дата которую ввел пользователь больше или меньше даты на момент вызова функции.
+    return: True - если с пользовательским вводом всё отлично, и False - если нет!"""
+
+    flag = False
+    try:
+        object_dt = datetime.strptime(user_date, '%d.%m.%Y')
+        flag = True
+    except ValueError:
+        return flag
+
+    if flag:
+        date_today = datetime.today()
+        if object_dt > date_today:
+            return False
+        return True
+
+def get_final_data(data_key: str, object_dt: datetime) -> dict:
+    """Задача функции - отрыть файл с датами, попробовать найти ключ и вернуть True or False
+    Возможные варианты развития событий:
+    1) Даты (ключа в словаре) нет! -> 1000% должны сделать запрос к API
+    2) Дата есть! -> запрос мы уже делали, НО!
+        а) если у ключа значение False -> на этом наш скрипт должен закончить работу
+        б) если у ключа значение True -> должны перейти к открытию файл...
+    data_key - это строка вида %d_%m_%Y
+    предполагаемое возвращаемое значение будет выглядеть как-то так:
+    {
+        'flag': True | False,
+        'data': данные,
+        'info': ...
+    }
+    """
+
+    final_data = {
+        'flag': None,
+        'data': None,
+        'info': None
+    }
+
+    with open('info_about_successful_requests.json', 'r', encoding='utf-8') as file:
+        info_about_all_dates: dict = json.load(file)
+
+    if data_key not in info_about_all_dates:
+        response = request_to_currency_api(object_dt, data_key)
+        if isinstance(response, bool):
+            final_data['flag'] = response
+            final_data['info'] = 'На указанную дату нет никакой информации. Попробуйте выбрать другую дату'
+        else:
+            final_data['flag'] = True
+            final_data['data'] = response
+            final_data['info'] = 'Запрос успешно обработан'
+
+    else:
+        flag = info_about_all_dates[data_key]
+        if flag:
+            final_data['data'] = open_and_read_data_from_file(data_key)
+            final_data['info'] = 'Запрос успешно обработан'
+            final_data['flag'] = True
+        else:
+            final_data['info'] = 'На указанную дату нет никакой информации.Выберите другую дату'
+            final_data['flag'] = False
+
+    return final_data
+
+def open_and_read_data_from_file(data_key: str) -> dict:
+    with open(f'data_base/{data_key}.json', 'r', encoding='utf-8') as file:
+        json_data_from_api = json.load(file)
+
+    return json_data_from_api['Valute']
+
+
+def request_to_currency_api(object_dt: datetime, date_key: str) -> bool | dict:
+    with open('info_about_successful_requests.json', 'r', encoding='utf-8') as file:
+        info_about_all_dates = json.load(file)
+
+    url = f'https://www.cbr-xml-daily.ru/archive/{object_dt.strftime("%Y/%m/%d")}/daily_json.js'
+    response = requests.get(url)
+    json_data_from_api = response.json()
+
+    if response.status_code != 200:
+        info_about_all_dates[date_key] = False
+    else:
+        info_about_all_dates[date_key] = True
+
+        with open(f'data_base/{date_key}.json', 'w', encoding='utf-8') as file:
+            json.dump(json_data_from_api, file, indent=4, ensure_ascii=False)
+
+    with open('info_about_successful_requests.json', 'w', encoding='utf-8') as file:
+        json.dump(info_about_all_dates, file, indent=4, ensure_ascii=False)
+
+    if info_about_all_dates[date_key]:
+        return json_data_from_api['Valute']
+
+    return False
+
+
+def get_left_and_right_border(data: str) -> dict:
+    date_time_obj = datetime.strptime(data, '%d.%m.%Y')
+    copy_dt = date_time_obj
+    result_borders = {
+        "left": None,
+        "right": None,
+    }
+
+    for i in ['left', 'right']:
+        date_time_obj = copy_dt
+        count, flag = 0, True
+        while flag:
+            if i == 'right':
+                date_time_obj += timedelta(days=1)
+                if date_time_obj > datetime.now():
+                    break
+
+            elif i == 'left':
+                date_time_obj -= timedelta(days=1)
+
+            result_response = request_to_currency_api(date_time_obj, date_time_obj.strftime('%d_%m_%Y'))
+            if isinstance(result_response, bool):
+                count += 1
+
+                if count >= 5:
+                    break
+                continue
+
+            result_borders[i] = date_time_obj.strftime('%d.%m.%Y')
+            flag = False
+
+    print(result_borders)
+    return result_borders
+
+
---
+++ main (3).py
@@ -0,0 +1,114 @@
+# import requests
+#
+# from requests.models import Response
+#
+# from datetime import datetime
+# import json
+#
+# from functions import *
+# from flask import Flask, render_template, request, redirect, url_for
+#
+# # date_from_input = input('Введи дату в формате dd.mm.yyyy: ')
+# #
+# # if check_correctly_date(date_from_input):
+# #     object_dt = datetime.strptime(date_from_input, '%d.%m.%Y')
+# #     date_key: str = object_dt.strftime('%d_%m_%Y')
+# #     result = get_final_data(date_key, object_dt)
+# #
+# #     if result['flag']:
+# #         print(result['info'], 'Они будут ниже:')
+# #         print(result['data'])
+# #     else:
+# #         print(result['info'])
+# #
+# # else:
+# #     print('error')
+#
+#
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def index():
+#     data = {
+#         'title': "Мой новый заголовок",
+#         'day': 'Введи день',
+#         'month': 'Введи месяц',
+#         'year': 'Введи год',
+#     }
+#     return render_template('index.html', **data)
+#
+#
+# @app.route('/submit', methods=["POST"])
+# def submit():
+#     date = request.form.get('date')
+#     # print(f'переменная date: {date}')
+#     if date is not None:
+#         return redirect(url_for('success', current_date=date))
+#
+#     day = request.form.get('day')
+#     month = request.form.get('month')
+#     year = request.form.get('year')
+#
+#     if not check_correctly_date(f'{day}.{month}.{year}'):
+#         return redirect(url_for('error'))
+#
+#     else:
+#         return redirect(url_for('success', current_date=f'{day}.{month}.{year}'))
+#
+#
+# @app.route('/success')
+# def success():
+#     data = {
+#         'title': "Курс валют",
+#     }
+#     current_date: str = request.args.get('current_date')
+#     object_dt = datetime.strptime(current_date, '%d.%m.%Y')
+#     date_key: str = object_dt.strftime('%d_%m_%Y')
+#     final_data = get_final_data(date_key, object_dt)
+#     if final_data['data'] is None:
+#         result_borders = get_left_and_right_border(current_date)
+#         count_date = 0
+#         if result_borders['left'] is not None:
+#             count_date += 1
+#         if result_borders['right'] is not None:
+#             count_date += 1
+#         data['left'] = result_borders['left']
+#         data['right'] = result_borders['right']
+#         data['count_date'] = count_date
+#         return render_template('two_button.html', **data)
+#     for key, value in final_data['data'].items():
+#         if value['Nominal'] != 1:
+#             new_value = value['Value'] / value['Nominal']
+#             value['Nominal'] = 1
+#             value['Value'] = round(new_value, 4)
+#     data['currency'] = final_data['data']
+#     return render_template('success.html', **data)
+#
+#
+# @app.route('/error')
+# def error():
+#     data = {
+#         'title': "курс валют",
+#     }
+#     return render_template('error.html')
+#
+#
+# @app.route('/test_currency')
+# def test_currency():
+#     data = {
+#         'title': "курс валюты на выбранную дату",
+#     }
+#     with open('data_base/11_07_2024.json', 'r', encoding='utf-8') as file:
+#         currency: dict = json.load(file)
+#
+#     data['currency'] = currency['Valute']
+#
+#     return render_template('test_currency.html', **data)
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
